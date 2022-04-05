56be74cb2aa0c2a7492313c3efccbbd64d2e9d86
Dom_monash <dwag0001@student.monash.edu>
2022-04-05 17:51:22 +1000

    stable API version

--- application.py
+++ application.py
@@ -29,13 +29,12 @@ def getData():
 
     def dynamic_query(fields):
         query = """SELECT e.event_name, a.activity_name, a.activity_type, e.participants_count, 
-            l.location_name, l.address_1 
-            FROM events e, activities a, locations l 
-            where e.activity_id = a.activity_id and e.location_id = l.location_id"""
+                    l.location_name, l.address_1 
+                    FROM events e, activities a, locations l 
+                    where e.activity_id = a.activity_id and e.location_id = l.location_id"""
         for key, value in fields.items():
             query += f" and {key_dict[key]} = '{value}'"
         query += ";"
-        print(query)
         return query
 
     ##### Future iterations will have security features here to prevent malicious attack
@@ -43,10 +42,10 @@ def getData():
 
     cur = mysql.connection.cursor()
 
-    print(request.method)
-    print(request.args)
+    params = dict(request.args)
+    print(params)
     try:
-        if request.args['init'] == 'activity':
+        if params['init'] == 'activity':
             query = """
                     SELECT a.activity_type
                     FROM activities a; 
@@ -54,35 +53,12 @@ def getData():
             cur.execute(query)
             return json.dumps(cur.fetchall())
 
-    except:
-        print(dynamic_query(request.args))
+    except KeyError:
         cur.execute(dynamic_query(request.args))
         return json.dumps(cur.fetchall())
 
-    #except:
-    #    print('Different error occurred')
-
-
-
-
-@app.route('/events', methods = ["POST", "GET"])
-def getEvents():
-    print(request.args)
-    query = """SELECT e.event_name, a.activity_name, a.activity_type, e.participants_count, 
-    l.location_name, l.address_1 
-    FROM events e, activities a, locations l 
-    where e.activity_id = a.activity_id and e.location_id = l.location_id"""
-    for key, value in request.args.items():
-        query += f" and {key} in {value.replace('[', '(').replace(']', ')')}"
-    query += ";"
-    print(query)
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    data = cur.fetchall()
-    cur.close()
-    print(json.dumps(data))
-    #return params
-    return json.dumps(data)
+    except:
+        print('Different error occurred')
 
 if __name__ == '__main__':
     app.run()
