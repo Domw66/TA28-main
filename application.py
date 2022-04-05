23317e435185d167548080303e42cbe3c16f5f5f
Dom_monash <dwag0001@student.monash.edu>
2022-04-05 17:02:12 +1000

    API testing

--- application.py
+++ application.py
@@ -4,11 +4,6 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/cardcanvas')
-def cardcanvas():
-    return render_template("cardcanvas.html")
-
-
 app.config['MYSQL_HOST'] = 'ta28.mysql.database.azure.com'
 app.config['MYSQL_USER'] = 'ta28'
 app.config['MYSQL_PASSWORD'] = 'Monash#123'
@@ -19,6 +14,46 @@ mysql = MySQL(app)
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html')
+
+@app.route('/api', methods = ["GET", "POST"])
+def getData():
+
+    def dynamic_query(fields):
+        query = """SELECT e.event_name, a.activity_name, a.activity_type, e.participants_count, 
+            l.location_name, l.address_1 
+            FROM events e, activities a, locations l 
+            where e.activity_id = a.activity_id and e.location_id = l.location_id"""
+        for key, value in fields.items():
+            query += f" and {key} in {value.replace('[', '(').replace(']', ')')}"
+        query += ";"
+        print(query)
+        return query
+
+    ##### Future iterations will have security features here to prevent malicious attack
+    # security()
+
+    cur = mysql.connection.cursor()
+
+    print(request.method)
+    print(request.args)
+    try:
+        if request.args['init'] == 'activity':
+            query = """
+                    SELECT a.activity_type
+                    FROM activities a; 
+                    """
+            cur.execute(query)
+            return json.dumps(cur.fetchall())
+
+    except NameError:
+        cur.execute(dynamic_query(request.args))
+        return json.dumps(cur.fetchall())
+
+    except:
+        print('Different error occurred')
+
+
+
 
 @app.route('/events', methods = ["POST", "GET"])
 def getEvents():
