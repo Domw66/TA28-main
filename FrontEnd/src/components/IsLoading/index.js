58d1b467f91c5bcc87ffcd24af96ce8b538a6092
James <jamz97114@gmail.com>
2022-04-06 11:42:38 +1000

    FrontEndv2

---
+++ FrontEnd/src/components/IsLoading/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div class="cssload-container">
+      <div class="cssload-arc">
+        <div class="cssload-arc-cube"></div>
+      </div>
+    </div>
+  )
+}
