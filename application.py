ec13648feb2c766bf5a9d242487947f4206dcbcf
Avinash P <apam0001@student.monash.edu>
2022-04-09 17:03:06 +1000

    09.04 New API as per Yiwen boyfriend specifications

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+from json import JSONEncoder
 from Classes import Query, Event, Button
 from flask import Flask, render_template, request, send_from_directory
 from flask_mysqldb import MySQL
@@ -45,6 +47,12 @@ for i, btn in enumerate(button_list):
     if i > 9:
         exec(f"b{btn} = Button('{btn}', 'time')")
 
+class DateTimeEncoder(JSONEncoder):
+        #Override the default method
+        def default(self, obj):
+            if isinstance(obj, (datetime.date, datetime.datetime)):
+                return obj.isoformat()
+
 #Render Homepage
 @app.route('/')
 def index():
@@ -90,6 +98,54 @@ def api():
 @app.route('/init')
 def init():
     return str(Button.format())
+
+# Render general data  API
+@app.route('/filter_page', methods = ["GET", "POST"])
+def filter_page():
+
+    # cursor object to access DB
+    cur = mysql.connection.cursor()
+    input_request = request.args
+    query_string_parameters = {}
+    mandatory_params = ['Activity_type', 'Location', 'Time', 'PageNum', 'PageSize']
+    default_values =  {'Activity_type': 'AllActivity', 'Location': 'AllLocation', 'Time': 'AllTime', 'PageNum' : '1', 'PageSize' : '5'}
+    for mandatory_param in mandatory_params:
+        if mandatory_param not in input_request:
+            query_string_parameters[mandatory_param] = default_values[mandatory_param]
+        else:
+            query_string_parameters[mandatory_param] = input_request[mandatory_param]    
+    print(query_string_parameters)
+
+    page_count_sql = """
+    select ceil(count(distinct e.event_image_path, e.event_name, 
+    e.event_date, e.event_start_time_24hr, e.participants_count, l.suburb)/{0}) as page_count 
+    from events e, locations l 
+    where e.location_id = l.location_id; 
+    """
+    cur.execute(page_count_sql.format(query_string_parameters['PageSize']))
+    row = cur.fetchone()
+    total_pages = int(row['page_count'])
+
+    records_sql = """
+    select distinct e.event_image_path, e.event_name, 
+    e.event_date, e.event_start_time_24hr, e.participants_count, l.suburb 
+    from events e, locations l 
+    where e.location_id = l.location_id limit {0}, {1}; 
+    """
+    page_num = int(query_string_parameters['PageNum'])
+    page_size = int(query_string_parameters['PageSize'])
+    cur.execute(records_sql.format((page_num - 1) * page_size, page_num * page_size))
+
+    response_template = { 
+        "TotalPage": None, 
+        "PageNum": None, 
+        "Data": []
+    }
+    response_template['TotalPage'] = total_pages
+    response_template['PageNum'] = page_num
+    response_template['Data'] = cur.fetchall()
+
+    return json.dumps(response_template, cls=DateTimeEncoder)
 
 if __name__ == '__main__':
     app.run()
